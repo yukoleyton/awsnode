5ffef6516735b84319146037b63a67221bd7601f
yukoleyton <samuelnt68u4@gmail.com>
2021-04-25 06:34:15 -0500

    EXITO!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Betsson RCTMRE OJALA CONSIGA LA CHAMBA CSMRE</h1>'))
+app.get('/', (req, res) => res.send('<h1>Buenos dias amorcito ❤❤❤❤❤❤😘😘😘😘😘😘😘 </h1>'))
 
 app.listen(3000);
 console.log('FUNCIONO CTMRE')
